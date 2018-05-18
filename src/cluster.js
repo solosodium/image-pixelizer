0d38efa83fe7256faa8b1e6b49d8ee84fc16815a
Hao Fu <fuhaoee@gmail.com>
2018-05-18 01:08:06 -0400

    Forgot a ().

---
+++ src/cluster.js
@@ -0,0 +1,52 @@
+(function() {
+
+    const Pixels = require('./pixels');
+
+    class Cluster {
+        
+        /**
+         * Default constructor.
+         * @param {Pixels} oldPixels pixel representation of the old image 
+         * @param {Pixels} newPixels pixel representation of the new image
+         */
+        constructor(oldPixels, newPixels) {
+            this.oldPixels = oldPixels;
+            this.newPixels = newPixels;
+            this.labels = [];
+            this.clusters = [];
+            this.initLabelsAndClusters();
+        }
+
+        initLabelsAndClusters() {
+            // Labels are for each individual old pixels, which indicate 
+            // which cluster (new pixel) it belongs to. The label is a 
+            // string of the format 'x,y', default value is 'null'.
+            for (let x=0; x<this.oldPixels.width; x++) {
+                for (let y=0; y<this.oldPixels.height; y++) {
+                    this.labels[x + y * this.oldPixels.width] = 'null';
+                }
+            }
+            // Clusters are for each individual new pixelx, which is the
+            // number of old pixels it includes. Default value is 0.
+            for (let x=0; x<this.newPixels.width; x++) {
+                for (let y=0; y<this.newPixels.height; y++) {
+                    this.clusters[x + y * this.newPixels.width] = 0;
+                }
+            }
+        }
+
+        cluster() {
+            // Calculate basic metrics.
+            let size = Math.round((this.oldPixels.width / this.newPixels.width + this.oldPixels.height / this.newPixels.height) / 2);
+
+        }
+
+        getResult() {
+            return this.newPixels;
+        }
+
+    }
+
+    module.exports = Cluster;
+
+})();
--- test/cluster-test.js
+++ test/cluster-test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 const Pixels = require('../src/pixels');
-const Core = require('../src/core');
+const Cluster = require('../src/cluster');
 
 describe('Cluster (cluster.js)', () => {
     
@@ -21,18 +21,12 @@ describe('Cluster (cluster.js)', () => {
     let oldPixels = new Pixels(10, 10, 1, mockImage);
     let newPixels = new Pixels(10, 10, 2, mockImage);
 
-    let test = new Core(oldPixels, newPixels);
-
-    //let clusters = new Clusters(oldPixels, newPixels);
-
-    console.log(oldPixels);
-
     it('constructor should initialize the corret values', () => {
-        //let cluster = new Clusters(oldPixels, newPixels);
-        // assert.deepEqual(cluster.oldPixels, oldPixels);
-        // assert.deepEqual(cluster.newPixels, newPixels);
-        // assert.equal(cluster.labels[0], 'null');
-        // assert.equal(cluster.clusters[0], 0);
+        let cluster = new Cluster(oldPixels, newPixels);
+        assert.deepEqual(cluster.oldPixels, oldPixels);
+        assert.deepEqual(cluster.newPixels, newPixels);
+        assert.equal(cluster.labels[0], 'null');
+        assert.equal(cluster.clusters[0], 0);
     });
 
 });
