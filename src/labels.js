2591a550ac7de2725fa7533759744cb37d2f0678
Hao Fu <fuhaoee@gmail.com>
2018-06-13 00:42:25 -0400

    Making labels class

--- src/labels.js
+++ src/labels.js
@@ -4,26 +4,43 @@
 
         /**
          * Default constructor.
-         * @param {number} width pixels width
-         * @param {number} height pixels height
+         * @param {number} width old pixels width
+         * @param {number} height old pixels height
          * @param {number} size new pixel size
          */
         constructor(width, height, size) {
-            this.width = width;
-            this.height = height;
+            this.oldWidth = width;
+            this.oldHeight = height;
             this.size = size;
-            this.labels = [];
-            // Initialize labels.
-            for (let x=0; x<width; x++) {
-                for (let y=0; y<height; y++) {
-                    this.labels[y * width + x] = {
-                        x: Math.floor(x / size),
-                        y: Math.floor(y / size),
+            // Create new pixels width and height.
+            this.newWidth = Math.ceil(this.oldWidth / this.size);
+            this.newHeight = Math.ceil(this.oldHeight / this.size);
+
+            console.log(this.newWidth, this.newHeight);
+
+            // Label is for each 
+            this.labels = Array(this.oldWidth)
+                .fill()
+                .map(() => Array(this.oldHeight));
+            this.counts = Array(this.newWidth)
+                .fill()
+                .map(() => Array(this.newHeight).fill(0));
+            // Initialize labels and counts.
+            for (let x=0; x<this.oldWidth; x++) {
+                for (let y=0; y<this.oldHeight; y++) {
+                    // Get new pixel position.
+                    let xx = Math.floor(x / size);
+                    let yy = Math.floor(y / size);
+                    // Set label.
+                    this.labels[x][y] = {
+                        x: xx,
+                        y: yy,
                         changed: true
                     };
+                    // Set count.
+                    this.counts[xx][yy] += 1;
                 }
             }
-            this.counts = [];
         }
 
         /**
@@ -32,9 +49,9 @@
          * @param {number} y 
          */
         getLabel(x, y) {
-            x = Math.max(0, Math.min(x, this.width));
-            y = Math.max(0, Math.min(x, this.height));
-            return this.labels[y * this.width + x];
+            x = Math.max(0, Math.min(x, this.oldWidth));
+            y = Math.max(0, Math.min(x, this.oldHeight));
+            return this.labels[x][y];
         }
 
         /**
@@ -43,13 +60,11 @@
          * @param {number} y 
          * @param {number} labelX 
          * @param {number} labelY 
-         * @param {number} forceChange 
          */
-        setLabel(x, y, labelX, labelY, forceChanged) {
+        setLabel(x, y, labelX, labelY) {
             let label = this.getLabel(x, y);
-            let changed = forceChanged || 
-                (label.x !== labelX && label.y !== labelY);
-            this.labels = {
+            let changed = (label.x !== labelX) && (label.y !== labelY);
+            this.labels[x][y] = {
                 x: labelX,
                 y: labelY,
                 changed: changed
--- test/labels-test.js
+++ test/labels-test.js
@@ -4,10 +4,8 @@ const Labels = require('../src/labels');
 describe('Labels (labels.js)', () => {
 
     it('constructor should initialize correct values', () => {
-        let labels = new Labels(200, 200);
-        assert.equal(labels.labels.length, 200 * 200);
+        let labels = new Labels(210, 210, 20);
         let label = labels.getLabel(20, 20);
-        assert.equal
     });
 
 });
