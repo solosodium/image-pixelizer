1c10a25c160ab777b1128b4e22a346a596045627
Hao Fu <fuhaoee@gmail.com>
2018-12-06 00:11:08 -0500

    Color clustering still problematic.

--- src/palette.js
+++ src/palette.js
@@ -25,18 +25,13 @@
 
 		/**
 		 * Reduce the number of colors in pixels.
-		 * @param {number} number final number of reduced colors
 		 * @return {Array{RGBA}} an array of reduced colors
 		 */
-		reduce(number) {
-            Log.info('Reducing number of colors to ' + number);
-			this.eliminate(number);
-            this.map();
-            return this.pixels;
-		}
-		
-		/** Create color groups. */
-		group(number) {
+		reduce() {
+			// If number of colors is not positve, do not reduce.
+			if (this.options.numberOfColors <= 0) {
+				return this.pixels;
+			}
 			// Initialize groups data structure.
 			this.groups = [];
 			this.groups.push({
@@ -45,12 +40,29 @@
 				oldList: [],
 				newList: []
 			});
-
-
+			// Loop to increase groups sizes to input number.
+			while (this.groups.length < this.options.numberOfColors) {
+				this.initGroups();
+				let iter = 0;
+				let change = this.assignGroups();
+				while (iter < 100000 && change > this.options.clusterThreshold) {
+					this.initGroups();
+					change = this.assignGroups();
+					iter++;
+					console.log("Iteration: " + iter + ", change is: " + change);
+				}
+				this.updateGroups();
+				this.splitGroups();
+				Log.info("Color number is " + this.groups.length);
+				
+			}
+			// Map pixels to groups and return pixels.
+            this.map();
+            return this.pixels;
 		}
 
 		/** Initialize groups for next iteration. */
-		initGroup() {
+		initGroups() {
 			for (let i=0; i<this.groups.length; i++) {
 				this.groups[i].oldList = this.groups[i].newList;
 				this.groups[i].newList = [];
@@ -58,10 +70,10 @@
 		}
 
 		/** Assign colors to groups. */
-		assignGroup() {
+		assignGroups() {
 			let colorChanged = 0;
 			for (let i=0; i<this.colors.length; i++) {
-				let minDiff = 1;
+				let minDiff = 2;
 				let groupIndex = 0;
 				for (let j=0; j<this.groups.length; j++) {
 					let diff = RGBA.difference(this.groups[j].mean, this.colors[i]);
@@ -70,22 +82,25 @@
 						groupIndex = j;
 					}
 				}
-				if (!this.groups[groupIndex].oldList.indexOf(this.colors[i])) {
+				if (this.groups[groupIndex].oldList.indexOf(this.colors[i]) < 0) {
 					colorChanged++;
 				}
 				this.groups[groupIndex].newList.push(this.colors[i]);
 			}
+
+			//console.log(colorChanged / this.colors.length);
+
 			return colorChanged / this.colors.length;
 		}
 
 		/** Update groups parameters. */
-		updateGroup() {
+		updateGroups() {
 			for (let i=0; i<this.groups.length; i++) {
 				let aggregate = new RGBA(0, 0, 0, 0);
 				let r2 = 0, g2 = 0, b2 = 0, a2 = 0;
-				for (let j=0; j<this.groups[i].newList.length; j++) {
-					let length = this.groups[i].newList.length;
-					RGBA.add(aggregate, 
+				let length = this.groups[i].newList.length;
+				for (let j=0; j<length; j++) {
+					aggregate = RGBA.add(aggregate, 
 						RGBA.scale(
 							this.groups[i].newList[j], 
 							1 / length));
@@ -109,13 +124,16 @@
 					Math.sqrt(b2 - aggregate.b * aggregate.b) + 
 					Math.sqrt(a2 - aggregate.a * aggregate.a)
 				) / 4;
+
+				
 			}
+			//console.log(this.groups[0]);
 		}
 
 		/** Split the group with the highest sd into 2. */
-		splitGroup() {
+		splitGroups() {
 			// Find group with the largest sd.
-			let maxSd = 0;
+			let maxSd = -1;
 			let group = null;
 			for (let i=0; i<this.groups.length; i++) {
 				if (this.groups[i].sd > maxSd) {
@@ -139,71 +157,20 @@
 				newList: []
 			});
 		}
-        
-        /** Eliminate the colors in pixels to a fixed number. */
-        eliminate(number) {// Skip if number is non-positive.
-			if (number <= 0) {
-				this.reducedColors = this.pixels.pixels;
-				return this.reducedColors;
-			}
-			// Transform pixels to weighted colors.
-			let weightedColors = this.pixels.pixels.map((pixel) => {
-				return { weight: 1, color: pixel };
-			});
-			// Weighted colors length has to be at least 2.
-			while (weightedColors.length > Math.max(1, number)) {
-				// Find the colors that are closest.
-				let minDiff = 1;
-				let closestColors = [];
-				for (let i=0; i<weightedColors.length; i++) {
-					for (let j=i+1; j<weightedColors.length; j++) {
-						let diff = RGBA.difference(
-							weightedColors[i].color,
-							weightedColors[j].color
-						);
-						if (diff <= minDiff) {
-							minDiff = diff;
-							closestColors = [
-								weightedColors[i],
-								weightedColors[j]
-							];
-						}
-					}
-				}
-				// Compute the reduced weighted color.
-				let reducedWeight = closestColors[0].weight + closestColors[1].weight;
-				let reducedColor = RGBA.add(
-					RGBA.scale(closestColors[0].color, closestColors[0].weight / reducedWeight),
-					RGBA.scale(closestColors[1].color, closestColors[1].weight / reducedWeight)
-				);
-				// Remove closest colors and add the reduced color.
-				weightedColors.splice(weightedColors.indexOf(closestColors[0]), 1);
-				weightedColors.splice(weightedColors.indexOf(closestColors[1]), 1);
-				weightedColors.push(
-					{ weight: reducedWeight, color: reducedColor }
-                );
-                Log.info('Number of colors is reduced to ' + weightedColors.length);
-            }
-            // Transform weighted colors to reduced colors.
-            this.reducedColors = weightedColors.map((weightedColor) => {
-                return weightedColor.color;
-			});
-			return this.reducedColors;
-        }
 
-        /** Map pixels to colors after elimination. */
+        /** Map pixels to color groups. */
         map() {
             for (let x=0; x<this.pixels.width; x++) {
                 for (let y=0; y<this.pixels.height; y++) {
-                    let minDiff = 1;
-                    let closestColor = this.pixels.getPixel(x, y).copy();
-                    for (let i=0; i<this.reducedColors.length; i++) {
+                    let minDiff = 2;
+                    let closestColor = this.pixels.getPixel(x, y);
+                    for (let i=0; i<this.groups.length; i++) {
                         let diff = RGBA.difference(
                             this.pixels.getPixel(x, y), 
-                            this.reducedColors[i]);
+                            this.groups[i].mean);
                         if (diff <= minDiff) {
                             minDiff = diff;
-                            closestColor = this.reducedColors[i];
+                            closestColor = this.groups[i].mean;
                         }
                     }
                     this.pixels.setPixel(x, y, closestColor);
